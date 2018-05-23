3a8a968774e82db1e89714f03229464de439fefd
Isma Perez <iperez@qmetric.co.uk>
2018-05-23 12:16:01 +0100

    added rules for fuzzylumpkins

--- tslint.js
+++ tslint.js
@@ -2,15 +2,18 @@ module.exports = {
   extends: 'tslint-config-airbnb',
   rules: {
     // diverge from airbnb: rules we do not want
-    'ter-arrow-parens': false,
-    'import-name': false,
-    'variable-name': false,
     'align': false,
+    'import-name': false,
     'max-line-length': false,
+    'object-literal-key-quotes': [true, 'as-needed'],
+    'quotemark': ['single', 'avoid-template', 'avoid-escape'],
+    'ter-arrow-parens': false,
+    'variable-name': false,
 
     // diverge from airbnb: rules we will want
-    'object-shorthand-properties-first': false,
     'object-curly-spacing': false,
+    'object-shorthand-properties-first': false,
     'trailing-comma': [false, {'esSpecCompliant': true}],
+    'radix': false,
   }
 };
